ba97ece27feac324f84695478e50cd7d49503714
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2023-08-23 20:06:43 -0500

    versión 1

---
+++ 20200123_20200101_HappyBirthday/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20200123_20200101_HappyBirthday"
+include(":app")
